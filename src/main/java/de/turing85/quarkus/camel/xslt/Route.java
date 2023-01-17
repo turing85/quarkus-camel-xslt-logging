7bbcf92cc10c14779c2bebcec266a5f5fe2568d6
Marco Bungart <marco.bungart@googlemail.com>
2023-01-17 20:30:13 +0100

    switched from camel-xslt-saxon to camel-xslt

--- src/main/java/de/turing85/quarkus/camel/xslt/Route.java
+++ src/main/java/de/turing85/quarkus/camel/xslt/Route.java
@@ -1,10 +1,10 @@
 package de.turing85.quarkus.camel.xslt;
 
 import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.platformHttp;
-import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.xsltSaxon;
+import static org.apache.camel.builder.endpoint.StaticEndpointBuilders.xslt;
 
 import javax.ws.rs.core.MediaType;
-
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 
 @SuppressWarnings("unused")
@@ -12,6 +12,7 @@ public class Route extends RouteBuilder {
   @Override
   public void configure() {
     from(platformHttp("/hello").consumes(MediaType.APPLICATION_XML))
-        .to(xsltSaxon("Transformer.xsl"));
+        .to(xslt("Transformer.xsl"))
+        .log("${exchangeProperty.%s}".formatted(Exchange.XSLT_WARNING));
   }
 }
